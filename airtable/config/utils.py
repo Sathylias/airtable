b14ab97211ac453822dbff667b8b1d8aecd8e3fb
Maxime DARAICHE <m.daraiche@linkbynet.com>
2022-12-23 10:38:34 -0500

    Refactored with Black

--- airtable/config/utils.py
+++ airtable/config/utils.py
@@ -4,12 +4,12 @@
 import os
 import yaml
 
+
 def load_yaml(yaml_file: str):
     """
     Read data from a yaml file
     :params str yaml_file: Specify the yaml file to be loaded into the environment
     :return: Returns a dictionary containing the yaml data
     """
-    with open(os.path.abspath(yaml_file), 'r', encoding='utf-8') as yaml_f:
+    with open(os.path.abspath(yaml_file), "r", encoding="utf-8") as yaml_f:
         return yaml.safe_load(yaml_f)
-        
